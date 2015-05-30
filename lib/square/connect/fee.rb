9e6964df32e1e55226f1649b8f644ec89dda3792
Mike Gorski <mike@namedpipe.net>
2015-05-30 09:21:21 -0500

    Correct grabbing of fees

--- lib/square/connect/fee.rb
+++ lib/square/connect/fee.rb
@@ -5,15 +5,20 @@ module Square
 
       def initialize(*args)
         super do |attributes|
+          self.merchant = if attributes[:merchant_id]
+            Merchant.new attributes[:merchant_id], access_token
+          else
+            Merchant.me access_token
+          end
           self.name = attributes[:name]
           self.calculation_phase = attributes[:calculation_phase]
           self.rate = attributes[:rate]
           self.adjustment_type = attributes[:adjustment_type]
           self.applies_to_custom_amounts = attributes[:applies_to_custom_amounts]
           self.type = attributes[:type]
-          self.id = attributes[:id]
           self.inclusion_type = attributes[:inclusion_type]
           self.enabled = attributes[:enabled]
+          self.endpoint = endpoint_for merchant.identifier, :fees, identifier
         end
       end
     end
